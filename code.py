13f1509018c5d0dcbae0f07d5eb67c8d900399c0
Shrman007658 <shraman99@gmail.com>
2019-12-04 23:39:40 +0530

    Escape key working

--- code.py
+++ code.py
@@ -11,11 +11,13 @@ count=0
 
 def detect(img):
     faces = f_c.detectMultiScale(img, 1.3, 5)
-    count = 0;
+    count = 0
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 5)
         count = count + 1
     if count > 1:
+
+        #Steps for minimizing the windows...
         keyboard.press(Key.cmd)
         keyboard.press('m')
         keyboard.release('m')
@@ -27,7 +29,12 @@ while(True):
     ret, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     detect(frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #Stop if the escape key is pressed.
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        keyboard.press(Key.alt_l)
+        keyboard.press(Key.tab)
+        keyboard.release(Key.alt_l)
         break
 
 cap.release()
